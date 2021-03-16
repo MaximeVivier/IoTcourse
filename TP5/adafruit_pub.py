bd9fba6fdca5ff935fdd6403dfa2f8d5ecc4745e
Maxime Vivier <maximevivier92@gmail.com>
2021-03-16 16:03:07 +0100

    code works

--- TP5/adafruit_pub.py
+++ TP5/adafruit_pub.py
@@ -2,9 +2,32 @@ from simple import MQTTClient
 from network import WLAN
 import machine
 import time
+import pycom as pc
 
-def sub_cb(topic, msg):
-   print(msg)
+pc.heartbeat(False)
+
+# Définition du dictionnaire de couleurs en fontion de leur nom
+colors = {
+    'vert' : 0x00aa00,
+    'rouge' : 0xaa0000,
+}
+
+# Fonction à appeler pour changer la couleur de la LED
+def changeColor(color):
+    hexa = colors[color]
+    pc.rgbled(hexa)
+    return()
+
+def callback_temp(topic, msg):
+    print(msg)
+
+def callback_switch(topic, msg):
+    if msg == b"OFF":
+        print(msg)
+        changeColor('rouge')
+    elif msg == b"ON":
+        print(msg)
+        changeColor('vert')
 
 adc = machine.ADC()
 apin = adc.channel(pin='P13',attn=machine.ADC.ATTN_11DB)
@@ -15,19 +38,30 @@ wlan.connect("Freebox-5FB8C7", auth=(WLAN.WPA2, "hispanarum#&-orthodoxi*-ructent
 while not wlan.isconnected():
     print("Connecting ...")
     machine.idle()
+    time.sleep_ms(500)
 print("Connected to WiFi\n")
 
-client = MQTTClient("ordi_adrien", "io.adafruit.com",user="adlaffargue", password="aio_eaGs44EvuauZdrbBiFee7o0fisJ8", port=1883)
 
-client.set_callback(sub_cb)
-client.connect()
-client.subscribe(topic="adlaffargue/feeds/tp-iot-mqtt")
 
+client_temp = MQTTClient("ordi_max1", "io.adafruit.com",user="adlaffargue", password="aio_uoKY313qNILljZDl1H7765uVtnI5", port=1883)
+client_switch = MQTTClient("ordi_max2", "io.adafruit.com",user="adlaffargue", password="aio_uoKY313qNILljZDl1H7765uVtnI5", port=1883)
+
+client_temp.set_callback(callback_temp)
+client_temp.connect()
+client_temp.subscribe(topic="adlaffargue/feeds/tp-temp")
+
+client_switch.set_callback(callback_switch)
+client_switch.connect()
+client_switch.subscribe(topic="adlaffargue/feeds/tp-switch")
+
+print("Initialization ok !\n")
 while True:
     temp = str(round(apin()/4096*40))
-    client.publish(topic="adlaffargue/feeds/tp-iot-mqtt", msg=temp)
+    client_temp.publish(topic="adlaffargue/feeds/tp-temp", msg=temp)
     time.sleep(1)
-    client.check_msg()
+    client_temp.check_msg()
+    client_switch.check_msg()
     time.sleep(1.5)
 
-client.disconnect()
+client_temp.disconnect()
+client_switch.disconnect()
